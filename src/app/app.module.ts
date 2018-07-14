daed1d6d77397c1e708d0664dad9ce580369955e
supawit <supawit.ubu@gmail.com>
2018-07-14 14:45:59 +0700

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { Route } from '../../node_modules/@angular/compiler/src/core';
+import { RouterModule,Routes} from '@angular/router';
 
+const Routes: Routes =[
+  {
+    path : 'dasboard',
+    component: DashboardComponent
+  },
+  {
+    path : '**',
+    component: HomeComponent
+  }
+];
+
+export const AppRouting = RouterModule.forRoot(Routes);
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
